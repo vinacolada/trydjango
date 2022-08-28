deb8058626357d848512ba3dede5bdc9dbfefebf
Vincent Araneta <aranetavincent74@gmail.com>
2022-08-28 17:48:04 +0800

    New Model Fields
    Django Model fields from documentation are applied to this.

---
+++ src/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+	#refer to documentation for model fields
+	title = models.CharField(max_length=120)
+	description = models.TextField(blank=True, null=True)
+	price = models.DecimalField(decimal_places=2, max_digits=1000)
+	summary = models.TextField()
