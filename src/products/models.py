51056cb430e637ca68655279be373e45edb22ca5
Vincent Araneta <aranetavincent74@gmail.com>
2022-08-28 18:04:38 +0800

    Change a Model
    Adding new fields to models and observing its effect to the database.

--- src/products/models.py
+++ src/products/models.py
@@ -6,4 +6,8 @@ class Product(models.Model):
 	title = models.CharField(max_length=120)
 	description = models.TextField(blank=True, null=True)
 	price = models.DecimalField(decimal_places=2, max_digits=1000)
-	summary = models.TextField()
+	# blank=False means required, null=False means cannot be empty in database
+	summary = models.TextField(blank=False, null=False)
+	# when adding a new field, use null=True to leave the new field empty for the older data
+	# or set default for all the previous data; you can use the command line to apply these
+	featured = models.BooleanField()
