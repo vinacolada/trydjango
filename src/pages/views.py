bc695c0ec61b64fb2edb5883f5da6d81e695eeeb
Vincent Araneta <aranetavincent74@gmail.com>
2022-08-29 19:05:17 +0800

    URL Routing and Requests
    Understanding requests and routing more pages in urls.py

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,5 +4,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs): # when you print args and kwargs you see a request (WSGIRequest) being passed as argument
+	print(args, kwargs)
+	print(request.user)
 	return HttpResponse("<h1>hello world</h1>")
+
+def contact_view(request, *args, **kwargs):
+	return HttpResponse("<h1>contacts page</h1>")
+
+def about_view(request, *args, **kwargs):
+	return HttpResponse("<h1>about page</h1>")
+
+def social_view(request, *args, **kwargs):
+	return HttpResponse("<h1>social page</h1>")
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view # import the actual view that you wanna use
+from pages.views import home_view, contact_view, about_view, social_view # import the actual view that you wanna use
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('contact/', contact_view),
+    path('about/', about_view),
+    path('social/', social_view),
     path('admin/', admin.site.urls),
 ]
